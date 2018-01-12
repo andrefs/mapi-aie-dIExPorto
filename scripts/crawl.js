cceb7afb824e4fbec255cf7ae63ea529a0575f6f
André Santos <andrefs@andrefs.com>
2018-01-12 18:01:23 +0000

    Crawling working. needs fix:latin1 encoding in zerozero

--- scripts/crawl.js
+++ scripts/crawl.js
@@ -1,63 +1,106 @@
 
 const Promise = require('bluebird');
-const parser = Promise.promisifyAll(require('rss-parser'));
+const Parser = require('rss-parser');
+const parser = Promise.promisifyAll(new Parser());
 const {mongoose,Article} = require('./lib/db');
+const md5 = require('md5');
+const {URL} = require('url');
 
 mongoose.connect('mongodb://localhost/aie_develop');
 
 const sources = {
   futebol365: {
+    slug: 'futebol365',
     rssUrl: 'http://feeds.feedburner.com/futebol365/noticias?format=xml',
+    urlTag: 'guid',
+    getOrigId: a => {
+      const url = new URL(a.url);
+      url.pathname.match(/^\/artigo\/(\d+)/);
+      return RegExp.$1;
+    }
   },
   maisfutebol: {
-    rssUrl: 'http://www.zerozero.pt/rss/noticias.php',
+    slug: 'maisfutebol',
+    rssUrl: 'http://www.maisfutebol.iol.pt/rss.xml',
+    urlTag: 'link',
+    getOrigId: a => {
+      const url = new URL(a.url);
+      url.pathname.match(/^\/(\w+)$/);
+      return md5(RegExp.$1);
+    }
   },
   zerozero: {
-    rssUrl: 'http://www.maisfutebol.iol.pt/rss.xml'
+    slug: 'zerozero',
+    rssUrl: 'http://www.zerozero.pt/rss/noticias.php',
+    urlTag: 'link',
+    getOrigId: a =>{
+      const url = new URL(a.url);
+      return parseInt(url.searchParams.get('id'),10);
+    }
   }
 };
 
 
-const getArticlesFromRss = (source, rssUrl) => {
-  return parser.parseURLAsync(rssUrl)
-    .then(parsed => {
+const getArticlesFromRss = source => {
+  return parser.parseURLAsync(source.rssUrl)
+    .then(feed => {
       const timestamp = new Date();
       let articles = [];
-      parsed.feed.entries.forEach(e => articles.push({
-        url: e.link || e.guid,
-        source,
-        pubDate: new Date(e.pubDate),
-        title: e.title,
-        crawl: {
-          timestamp
-        }
-      }));
+      feed.items.forEach(e => {
+      //console.log('XXXXXXXXXXx 0', e)
+        let article = {
+          url: e[source.urlTag],
+          source: source.slug,
+          pubDate: new Date(e.pubDate),
+          title: e.title,
+          crawl: {
+            timestamp
+          }
+        };
+        article.origId = source.getOrigId(article);
+        articles.push(article);
+      });
+      //console.log('XXXXXXXXXXx 1', articles)
       return articles;
     });
 };
 
-const crawlRss = (source, rssUrl) => {
-  return getArticlesFromRss(source, rssUrl)
+const crawlRss = source => {
+  return getArticlesFromRss(source)
     .then(articles => {
-      console.info('Saving',articles.length,'articles from',source);
-      return Article.create(articles);
-    })
-    .then(created => {
-      console.info('\tcreated:', created);
-      return created;
-    })
-    .catch(failed => console.warn('\tfailed',failed));
+      return createArticles(source, articles);
+    });
 };
 
 const crawlAllRss = (sources) => {
   let promises = [];
-  Object.keys(sources).forEach(s => {
-    promises.push(crawlRss(s, sources[s].rssUrl));
+  Object.values(sources).forEach(s => {
+    promises.push(crawlRss(s))
   });
   return Promise.all(promises);
 }
 
 crawlAllRss(sources)
-  .then(x => { console.log('XXXXXXXX 8', x); })
   .finally(() => mongoose.disconnect());
+
+
+const createArticles = (source, articles) => {
+  const query = {
+    url: {$in: articles.map(a => a.url)}
+  };
+  return Article.find(query).select('url').lean().exec()
+    .then(existingArticles => {
+      const existingUrls = existingArticles.map(a => a.url);
+      const newArticles  = articles.filter(a => !existingUrls.includes(a.url));
+      console.info('Found',articles.length,'articles from',source.slug);
+      if(!newArticles){
+        console.log('  no new articles');r
+        return [];
+      }
+      else {
+        console.log('  saving',newArticles.length,'new');
+        return Article.create(newArticles);
+      }
+    });
+};
 
