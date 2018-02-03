e15295a16c137b6763cd34a36a18255d8212eb5e
André Santos <andrefs@andrefs.com>
2018-02-03 19:42:23 +0000

    relextr clean up

--- scripts/relextr.js
+++ scripts/relextr.js
@@ -35,85 +35,25 @@ const getAllArticles = () => {
 //   }
 // };
 
-const emprestimoRule = {
-  scope: 'sentence',
-  rule: {
-    ordered: [
-      {tag: 'NP00O00'},
-      {pos: 'verb', lemma: 'emprestar', person: 3},
-      {tag: 'NP00SP0'},
-      {tag: 'NP00O00'}
-    ]
-  }
-};
-
-const empRule = new RelExRule([
+const emprestimoRule = new RelExRule([
   {tag: 'NP00O00'},
   {pos: 'verb', lemma: 'emprestar', person: 3},
   {tag: 'NP00SP0'},
   {tag: 'NP00O00'}
 ]);
 
-const arbitroRule = {
-  rule: {
-    ordered: [
-      {tag: 'NP00SP0'},
-      {lemma:'criticar'},
-      {lemma:'arbitragem'},
-    ]
-  }
-};
-
-// const matchSel = (sel, obj) => {
-//   let res = true;
-//   Object.keys(sel).forEach(key => {
-//     if(key === '$or'){ res = res && _matchOr(sel.$or, obj); }
-//     else { res = res && sel[key] === obj[key]; }
-//   });
-//   return res;
-// }
-// 
-// _matchOr = (selectors, obj) => {
-//   let res = false;
-//   selectors.forEach(sel => {
-//     res = res || matchSel(sel, obj);
-//   });
-//   return res;
-// }
-// 
-// const matchOrdered = (sentence, selectors) => {
-//   //console.log('XXXXXXXXXXXX 4', sentence, selectors);
-//   let i=0;
-//   let j;
-//   let matched = [];
-//   selectors.forEach(sel => {
-//     let j=i;
-//     while(j < sentence.length){
-//       if(matchSel(sel, sentence[j])){
-//         matched.push({sel, token: sentence[j]});
-//         i = j+1;
-//         return;
-//       } else { j++; }
-//     }
-//   });
-// 
-//   if(selectors.length === matched.length){
-//     console.log('XXXXXXXX 2', matched);
-//   }
-//   return selectors.length === matched.length;
-// }
+const arbitroRule = new RelExRule([
+  {tag: 'NP00SP0'},
+  {lemma:'criticar'},
+  {lemma:'arbitragem'},
+]);
 
 getAllArticles()
-  .then(articles => {
-    //const bls = Object.values(gazetteers).map(g => g.blacklist);
-    //return Promise.resolve(gazetteersBlacklist(bls, articles));
-    return Promise.resolve(runGazetteers(articles));
-  })
+  .then(articles => Promise.resolve(runGazetteers(articles)))
   .then(articles => {
     articles.forEach(a => {
       a.nlp.freeling.sentences.forEach(s => {
-        if(empRule.matchOrdered(s.tokens)){
-        //if(matchOrdered(s.tokens, emprestimoRule.rule.ordered)){
+        if(emprestimoRule.matchOrdered(s.tokens)){
           console.log('XXXXXXXXXx 8', s.id, a.url);
         } else {
           //console.log('XXXXXXXXXx 9');
@@ -121,5 +61,5 @@ getAllArticles()
       });
     });
     console.log('XXXXXXXXXX 99 finished');
-    //articles.filter(
+    mongoose.disconnect();
   });
