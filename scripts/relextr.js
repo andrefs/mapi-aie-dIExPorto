1a51ea136dfe6bb56e6daf2d7249c8a045934393
André Santos <andrefs@andrefs.com>
2018-02-03 21:50:40 +0000

    relextr processing all articles with skip + limit

--- scripts/relextr.js
+++ scripts/relextr.js
@@ -11,13 +11,6 @@ mongoose.connect('mongodb://localhost/aie_develop');
 
 let currentRequests = {};
 
-const getAllArticles = () => {
-  const query  = {'nlp.status':'success'};
-  //const query  = {url: 'http://www.maisfutebol.iol.pt/liga/vitoria-setubal/fc-porto-andre-pereira-a-caminho-de-setubal'};
-  const fields = 'nlp title url origId fetch.lead fetch.text';
-
-  return Article.find(query).select(fields).limit(1000).lean().exec();
-}
 
 // const emprestimoRule = {
 //   scope: 'sentence',
@@ -48,18 +41,32 @@ const arbitroRule = new RelExRule([
   {lemma:'arbitragem'},
 ]);
 
-getAllArticles()
-  .then(articles => Promise.resolve(runGazetteers(articles)))
-  .then(articles => {
-    articles.forEach(a => {
-      a.nlp.freeling.sentences.forEach(s => {
-        if(emprestimoRule.matchOrdered(s.tokens)){
-          console.log('XXXXXXXXXx 8', s.id, a.url);
-        } else {
-          //console.log('XXXXXXXXXx 9');
-        }
+
+const query  = {'nlp.status':'success'};
+//const query  = {url: 'http://www.maisfutebol.iol.pt/liga/vitoria-setubal/fc-porto-andre-pereira-a-caminho-de-setubal'};
+
+Article.count(query).then(total => {
+  console.log('Extracting relationships from',total,'articles');
+  const limit = 100;
+  let promises = [];
+  const iters = Math.ceil(total/limit);
+  for(let i=0; i<iters; i++){
+    promises.push(() => Article.find(query).select('origId url nlp title fetch.lead fetch.text').skip(i*limit).limit(limit).lean().exec());
+  }
+
+  return Promise.each(promises, getArticles => {
+    return getArticles().then(articles => {
+      articles.forEach(a => {
+        a.nlp.freeling.sentences.forEach(s => {
+          if(emprestimoRule.matchOrdered(s.tokens)){
+            console.log('XXXXXXXXXx 8', s.id, a.url);
+          } else {
+            //console.log('XXXXXXXXXx 9');
+          }
+        });
       });
     });
-    console.log('XXXXXXXXXX 99 finished');
-    mongoose.disconnect();
-  });
+  })
+  .then(() => { mongoose.disconnect(); });
+});
+
